74d35a13caa664ba17ef5eb65339a2dc73954bd8
lohitha-codes <lohithasaragada349@gmail.com>
2026-07-13 23:54:53 +0530

    Add files via upload

---
+++ print_matrix.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int a[2][3],i,j;
+    printf("enter elements of matrix: ");
+    for(i=0;i<2;i++){
+        for(j=0;j<3;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("matrix is: \n");
+    for(i=0;i<2;i++){
+        for(j=0;j<3;j++){
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
