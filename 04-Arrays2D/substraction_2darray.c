0b99ffb400c0b5a22a5270421e19d7ae7f7511bf
lohitha-codes <lohithasaragada349@gmail.com>
2026-07-19 22:55:40 +0530

    Update substraction_2darray.c

--- 04-Arrays2D/substraction_2darray.c
+++ 04-Arrays2D/substraction_2darray.c
@@ -30,7 +30,7 @@ int main(){
             arr3[i][j] = arr1[i][j]-arr2[i][j];
       }
     }
-    printf("addition matrix is :\n");
+    printf("substraction  matrix is :\n");
     for(i=0;i<3;i++){
         for(j=0;j<3;j++){
 printf("%d\t",arr3[i][j]);
